62414f08559f4f90f758d412e1b1059dd25792ce
inerciamodaenmovimiento <inerciamodaenmovimiento@gmail.com>
2026-04-17 21:30:21 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'daze-black': '#0A0A0A',
+        'daze-white': '#F5F5F5',
+        'daze-gray': '#E2E2E2',
+      },
+      fontFamily: {
+        'display': ['Inter', 'sans-serif'], // O la fuente editorial que elijas
+      },
+      letterSpacing: {
+        'tighter': '-0.05em',
+      }
+    },
+  },
+}
